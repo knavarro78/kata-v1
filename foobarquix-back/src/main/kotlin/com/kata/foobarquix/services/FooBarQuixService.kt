d8f462143862120eea34e1c8ca35a266bf33ecfd
segouhmid <se.gouhmid@outlook.com>
2020-12-03 15:57:28 +0100

    implement service method

--- foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
+++ foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
@@ -6,7 +6,17 @@ import org.springframework.stereotype.Component
 class FooBarQuixService {
 
     fun convertNumber(inputNumber: Int): String {
-        return ""
+        var result = ""
+        if(inputNumber % 3 == 0) result+= "Foo"
+        if(inputNumber % 5 == 0) result+= "Bar"
+        var inputNumberToString = inputNumber.toString();
+        for(i in 0.. inputNumberToString.length -1){
+            if(inputNumberToString[i].equals('3')) result+= "Foo"
+            else if(inputNumberToString[i].equals('5')) result+= "Bar"
+            else if(inputNumberToString[i].equals('7')) result+= "Quix"
+            else if(inputNumber % 5 != 0 && inputNumber % 3 != 0)  result+=inputNumberToString[i]
+        }
+        return result;
     }
 
 }
--- foobarquix-back/src/test/kotlin/com/kata/foobarquix/FooBarQuixApplicationTests.kt
+++ foobarquix-back/src/test/kotlin/com/kata/foobarquix/FooBarQuixApplicationTests.kt
@@ -8,68 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class FooBarQuixApplicationTests {
-	@Autowired
-	lateinit var fooBarQuixService: FooBarQuixService
 	@Test
 	fun contextLoads() {
-	}
-
-	@Test
-	fun convertNumber_should_return_1() {
-		val result = "1"
-		Assertions.assertThat(fooBarQuixService.convertNumber(1)).isEqualTo(result)
-	}
-	@Test
-	fun convertNumber_should_return_FooFoo() {
-		val result = "FooFoo"
-		Assertions.assertThat(fooBarQuixService.convertNumber(3)).isEqualTo(result)
-	}
-	@Test
-	fun convertNumber_should_return_BarBar() {
-		val result = "BarBar"
-		Assertions.assertThat(fooBarQuixService.convertNumber(5)).isEqualTo(result)
-	}
-
-	@Test
-	fun convertNumber_should_return_Quix() {
-		val result = "Quix"
-		Assertions.assertThat(fooBarQuixService.convertNumber(7)).isEqualTo(result)
-	}
-
-	@Test
-	fun convertNumber_should_return_Foo() {
-		val result = "Foo"
-		Assertions.assertThat(fooBarQuixService.convertNumber(9)).isEqualTo(result)
-	}
-
-	@Test
-	fun convertNumber_should_return_FooBar() {
-		val result = "FooBar"
-		Assertions.assertThat(fooBarQuixService.convertNumber(51)).isEqualTo(result)
-	}
-
-	@Test
-	fun convertNumber_should_return_BarFoo() {
-		val result = "BarFoo"
-		Assertions.assertThat(fooBarQuixService.convertNumber(53)).isEqualTo(result)
-	}
-
-	@Test
-	fun convertNumber_should_return_FooFooFoo() {
-		val result = "FooFooFoo"
-		Assertions.assertThat(fooBarQuixService.convertNumber(33)).isEqualTo(result)
-	}
-
-	@Test
-	fun convertNumber_should_return_FooQuix() {
-		val result = "FooQuix"
-		Assertions.assertThat(fooBarQuixService.convertNumber(27)).isEqualTo(result)
-	}
-
-	@Test
-	fun convertNumber_should_return_FooBarBar() {
-		val result = "FooBarBar"
-		Assertions.assertThat(fooBarQuixService.convertNumber(15)).isEqualTo(result)
 	}
 
 }
